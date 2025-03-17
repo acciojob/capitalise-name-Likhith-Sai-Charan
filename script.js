669be34681c1d93484d4f3035c60871c151bb921
Likhith Sai Charan Jupudi <143252814+Likhith-Sai-Charan@users.noreply.github.com>
2025-03-17 19:10:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+
+const Fname = document.getElementById("fname");
+
+Fname.addEventListener("blur", () => {
+    Fname.value = capitaliseName(Fname.value);
+});
+
+function capitaliseName(fname) {
+    return fname.toUpperCase();
+}
